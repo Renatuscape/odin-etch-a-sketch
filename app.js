6ee0327bd62cd94e3b02d522c8149ad6e41d5dc7
Renatuscape <silarcta@gmail.com>
2024-11-19 12:21:39 +0100

    Adds erase feature

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const container = document.getElementById("container");
+const eraseButton = document.getElementById("erase")
+let erasing = false;
+
+eraseButton.value = "Toggle Erase";
+eraseButton.onclick = toggleErase;
+
+function toggleErase() {
+  erasing = !erasing;
+  eraseButton.textContent = erasing ?  "Erase Mode" : "Drawing Mode";
+}
 
 function makeRows(rows, cols) {
-    for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      container.appendChild(cell).className = "grid-item";
-    };
-  };
-  
-  makeRows(16, 16);
+  // Clear existing grid items
+  container.innerHTML = '';
+
+  // Create new grid items
+  for (let i = 0; i < (rows * cols); i++) {
+    const cell = document.createElement("div");
+    cell.className = "grid-item";
+
+    // Add click event listener
+    cell.addEventListener('mouseover', function () {
+      if (!erasing) {
+        this.classList.add('active');
+      }
+      else {
+        this.classList.remove('active');
+      }
+    });
+
+    container.appendChild(cell);
+  }
+}
+
+makeRows(16, 16);
