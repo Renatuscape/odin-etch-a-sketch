09973a681b6004dd2aacf13b21831a1e07e5d6b9
Renatuscape <silarcta@gmail.com>
2024-11-20 10:23:33 +0100

    Adds styling to indicate which brush setting is active

--- brushController.js
+++ brushController.js
@@ -1,5 +1,6 @@
 window.brushStrength = 0.1;
 const settingsContainer = document.getElementById("brush-settings");
+const buttons = [];
 
 function buildSettings() {
     settingsContainer.innerHTML = '';
@@ -9,15 +10,41 @@ function buildSettings() {
         const button = document.createElement("button");
         button.className = 'brush-opacity-button';
         button.title = brushStrength;
-        button.style.opacity = brushStrength;
         brushStrength = Math.round((brushStrength + 0.1) * 10) / 10;
 
         settingsContainer.appendChild(button);
 
+        const buttonFill = document.createElement("div");
+        buttonFill.className = 'brush-button-fill';
+        buttonFill.style.opacity = brushStrength;
+        button.appendChild(buttonFill);
+
         button.onclick = () => {
-            window.brushStrength = parseFloat(value);
+            window.brushStrength = parseFloat(button.title);
+            button.appendChild(buttonFill);
+            setActive(button);
         }
+
+        buttons.push(button);
+
+                // Set the first button as active
+                if (i === 0) {
+                    setActive(button);
+                }
     }
+
+    console.table(buttons);
+}
+
+function setActive(clickedButton) {
+    // Remove active class from all buttons
+    buttons.forEach((button) => {
+        button.className = 'brush-opacity-button';
+    });
+    
+    // Add active class to clicked button
+    clickedButton.className = 'brush-opacity-button active';
+    console.log("Activated button with strength:", clickedButton.title);
 }
 
 buildSettings();
